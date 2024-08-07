0d8adb5e040c531f43b3ec92633f63b6eeb997c3
Hy <107110503+whanyu1212@users.noreply.github.com>
2024-08-07 12:39:20 +0000

    set up header and nav bar for respectively sub components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path: 'home', 'component': HomeComponent},
+  {path: 'portfolio', 'component': PortfolioComponent},
+  {path: 'resume', 'component': ResumeComponent},
+  {path: 'contact', 'component': ContactComponent},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
